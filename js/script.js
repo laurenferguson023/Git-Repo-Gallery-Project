4811c15e30b6f6ec81d896035da531410f9b65c0
Lauren Ferguson <laurenferguson023@gmail.com>
2025-02-12 14:09:26 -0500

    Added Fetch Repo Data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const overview = document.querySelector(".overview");
 const username = "laurenferguson023";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -25,5 +26,20 @@ const displayUserInfo = function (data) {
     `;
     
 overview.append(div);
+gitRepos();
+};
 
+const gitRepos = async function () {
+    const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=update&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li"); 
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
